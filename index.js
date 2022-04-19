5f9ddb6d7288e41095d2b0b997168b99a95ebf76
Arun3sh <m.aruneshwar@gmail.com>
2022-04-19 22:57:55 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { MongoClient } from 'mongodb';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT;
+const MONGO_URL = process.env.MONGO_URL;
+
+async function createConnection() {
+	const client = new MongoClient(MONGO_URL);
+	await client.connect();
+	console.log('mongo');
+	return client;
+}
+
+export const client = await createConnection();
+
+app.get('/', (request, response) => {
+	response.send('eCommerce');
+});
+
+app.listen(PORT, () => console.log('server started', PORT));
